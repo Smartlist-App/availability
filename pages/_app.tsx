5e9008f8bdf4d9c05fc0101712a46cbb6f83397c
_manu.codes <manusvathgurudath@gmail.com>
2022-12-27 23:55:55 +0000

    W

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,6 +23,10 @@ function Navbar() {
   return (
     <>
       <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
         <title>Availability &bull; A product of Carbon</title>
       </Head>
       <AppBar
