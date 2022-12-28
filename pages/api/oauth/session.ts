db0959cc75623d3a7b8308660cdb3b8d44cac46d
_manu.codes <manusvathgurudath@gmail.com>
2022-12-28 03:24:42 +0000

    W

--- pages/api/oauth/session.ts
+++ pages/api/oauth/session.ts
@@ -1,9 +1,9 @@
-import jwt from "jsonwebtoken";
+import jwt, { Secret } from "jsonwebtoken";
 
 export default async function handler(req: any, res: any) {
   try {
     const { session }: any = req.cookies;
-    const decoded = jwt.verify(session, process.env.SECRET_COOKIE_PASSWORD);
+    const decoded = jwt.verify(session, process.env.SECRET_COOKIE_PASSWORD as Secret);
     res.json(decoded);
   } catch (e) {
     res.json({ error: "Something went wrong" });
